fe10c406433a320c4ed25eb3ff85f0f726307c02
bw6657891-glitch <bw6657891@gmail.com>
2026-02-01 14:45:48 +0800

    Delete test.js

--- test.js
+++
@@ -1,9 +0,0 @@
-// 测试Vercel是否能识别这个文件
-module.exports = (req, res) => {
-    res.json({ 
-        message: "直接测试成功",
-        timestamp: new Date().toISOString(),
-        url: req.url,
-        method: req.method
-    });
-};
